0682eb998bc6900a7ced6539fecad38b606d70db
Noah Dahms <noahdahms@gmail.com>
2024-03-12 19:53:12 +0100

    added nvim-spectre plugin

---
+++ lua/custom/plugins/spectre.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'nvim-pack/nvim-spectre',
+    build = false,
+    cmd = 'Spectre',
+    opts = { open_cmd = 'noswapfile vnew' },
+    -- stylua: ignore
+    keys = {
+      { "<leader>srp", function() require("spectre").open() end, desc = "[S]earch [R]e[P]lace in files (Spectre)" },
+    },
+  },
+}
