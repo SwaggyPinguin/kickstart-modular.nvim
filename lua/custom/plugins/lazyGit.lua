af034e374f76a32331e33ab7e86b249cd0f6416f
Noah Dahms <noahdahms@gmail.com>
2024-03-13 16:25:28 +0100

    added lazygit plugin

---
+++ lua/custom/plugins/lazyGit.lua
@@ -0,0 +1,27 @@
+return {
+  'kdheepak/lazygit.nvim',
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+  },
+  init = function()
+    vim.keymap.set('n', '<leader>gl', '<cmd>LazyGit<CR>', { desc = '[G]it [L]azyGit' })
+    -- vim.keymap.set('n', '<leader>gc', '<cmd>LazyGitConfig<CR>', { desc = '[G]it LazyGit [C]onfig' })
+    -- vim.keymap.set('n', '<leader>gf', '<cmd>LazyGitCurrentFile<CR>', { desc = '[G]it LazyGit [F]ile' })
+    -- vim.keymap.set('n', '<leader>gF', '<cmd>LazyGitFilter<CR>', { desc = '[G]it LazyGit [F]ilter' })
+    -- vim.keymap.set('n', '<leader>gC', '<cmd>LazyGitFilterCurrentFile<CR>', { desc = '[G]it LazyGit [C]urrent [F]ile' })
+
+    vim.keymap.set('n', '<leader>gr', function()
+      require('telescope').extensions.lazygit.lazygit()
+    end, { desc = '[G]it LazyGit [R]epositories' })
+  end,
+}
+-- vim: ts=2 sts=2 sw=2 et
