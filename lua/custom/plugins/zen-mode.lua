5edc2bf8c562d8d03cc0506924de2e2453138503
Noah <noahdahms@gmail.com>
2024-03-15 23:31:17 +0100

    added zen-mode plugin

---
+++ lua/custom/plugins/zen-mode.lua
@@ -0,0 +1,26 @@
+return {
+  'folke/zen-mode.nvim',
+  opts = {
+    plugins = {
+      gitsigns = { enabled = true },
+      tmux = { enabled = false }, -- add this to tmux.conf -> set-option -g allow-passthrough on
+      twilight = { enabled = true },
+    },
+  },
+  -- init = function()
+  --   -- vim.keymap.set('n', '<leader>z', ':ZenMode<CR>', { silent = true, desc = '[Z]en Mode' })
+  -- end,
+  keys = {
+    {
+      '<leader>z',
+      function()
+        require('zen-mode').toggle {
+          window = {
+            width = 0.80, -- width will be XX% of the editor screen
+          },
+        }
+      end,
+      desc = '[Z]en Mode',
+    },
+  },
+}
