9d1d05e647715bd2c17def1de33d80fbb17f5289
Noah <noahdahms@gmail.com>
2024-03-15 23:30:54 +0100

    changed lualine theme to gruvbox-material

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -6,6 +6,10 @@ return {
     local lazy_status = require 'lazy.status'
 
     lualine.setup {
+      options = {
+        -- theme = 'gruvbox',
+        theme = 'gruvbox-material',
+      },
       sections = {
         lualine_x = {
           { lazy_status.updates, cond = lazy_status.has_updates, color = { fg = '#ff9e64' } },
