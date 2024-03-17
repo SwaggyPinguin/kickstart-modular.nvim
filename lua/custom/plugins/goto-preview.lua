681a16db4be0391324aa760679e232eb5c65bc19
Noah <noahdahms@gmail.com>
2024-03-17 19:56:28 +0100

    added goto-preview plugin

---
+++ lua/custom/plugins/goto-preview.lua
@@ -0,0 +1,25 @@
+return {
+  'rmagatti/goto-preview',
+  opts = {},
+  -- config = function()
+  --   require('goto-preview').setup {}
+  -- end,
+  init = function()
+    vim.keymap.set('n', 'gpd', ':lua require("goto-preview").goto_preview_definition()<CR>', { silent = true, desc = '[G]oto [P]review [D]efinition' })
+    vim.keymap.set('n', 'gpi', ':lua require("goto-preview").goto_preview_implementation()<CR>', { silent = true, desc = '[G]oto [P]review [I]mplementation' })
+    vim.keymap.set(
+      'n',
+      'gpt',
+      ':lua require("goto-preview").goto_preview_type_definition()<CR>',
+      { silent = true, desc = '[G]oto [P]review [T]ype [D]efinition' }
+    )
+    vim.keymap.set('n', 'gpr', ':lua require("goto-preview").goto_preview_references()<CR>', { silent = true, desc = '[G]oto [P]review [R]eferences' })
+    vim.keymap.set('n', 'gpD', ':lau require("goto-preview).goto_preview_declaration()<CR>', { silent = true, desc = '[G]oto [P]review [D]eclaration' })
+    vim.keymap.set(
+      'n',
+      'gpc',
+      ':lua require("goto-preview").close_all_win({ skip_curr_window = true })<CR>',
+      { silent = true, desc = '[G]oto [P]review [C]lose all windows' }
+    )
+  end,
+}
