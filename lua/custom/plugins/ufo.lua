8a345e75b80fa9b59267bc72274dc5e26334da30
Noah <noahdahms@gmail.com>
2024-03-21 23:06:37 +0100

    ufo hide fold indicator

--- lua/custom/plugins/ufo.lua
+++ lua/custom/plugins/ufo.lua
@@ -58,7 +58,7 @@ return {
     end,
   },
   init = function()
-    vim.o.foldcolumn = '1' -- '0' is not bad
+    vim.o.foldcolumn = '0' -- '0' is not bad '1' to show fold indicator
     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
