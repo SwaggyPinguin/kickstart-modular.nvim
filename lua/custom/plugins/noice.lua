1df2c7c7aef7fb0b4376a667805eac2294252911
Noah <noahdahms@gmail.com>
2024-03-21 23:07:41 +0100

    added notify plugin

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,58 +1,40 @@
-return {
-  {
-    -- 'folke/noice.nvim',
-    -- event = 'VeryLazy',
-    -- -- config = function()
-    -- --   require 'custom.configs.noice'
-    -- -- end,
-    -- opts = {
-    --   -- stylua: ignore
-    --   keys = {
-    --     { "<S-Enter>",   function() require("noice").redirect(vim.fn.getcmdline()) end,                 mode = "c",                 desc = "Redirect Cmdline" },
-    --     { "<leader>snl", function() require("noice").cmd("last") end,                                   desc = "Noice Last Message" },
-    --     { "<leader>snh", function() require("noice").cmd("history") end,                                desc = "Noice History" },
-    --     { "<leader>sna", function() require("noice").cmd("all") end,                                    desc = "Noice All" },
-    --     { "<leader>snd", function() require("noice").cmd("dismiss") end,                                desc = "Dismiss All" },
-    --     { "<c-f>",       function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,              expr = true,              desc = "Scroll forward",  mode = { "i", "n", "s" } },
-    --     { "<c-b>",       function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,              expr = true,              desc = "Scroll backward", mode = { "i", "n", "s" } },
-    --   },
-    -- },
-    -- dependencies = {
-    --   {
-    --     'MunifTanjim/nui.nvim',
-    --     lazy = true,
-    --   },
-    --   {
-    --     'rcarriga/nvim-notify',
-    --     config = function()
-    --       require('notify').setup {
-    --         background_colour = '#000000',
-    --       }
-    --     end,
-    --     keys = {
-    --       {
-    --         '<leader>un',
-    --         function()
-    --           require('notify').dismiss { silent = true, pending = true }
-    --         end,
-    --         desc = 'Dismiss all Notifications',
-    --       },
-    --     },
-    --     opts = {
-    --       timeout = 3000,
-    --       max_height = function()
-    --         return math.floor(vim.o.lines * 0.75)
-    --       end,
-    --       max_width = function()
-    --         return math.floor(vim.o.columns * 0.75)
-    --       end,
-    --       on_open = function(win)
-    --         vim.api.nvim_win_set_config(win, { zindex = 100 })
-    --       end,
-    --     },
-    --     lazy = true,
-    --   },
-    -- },
-  },
-}
+return {}
+-- return {
+--   {
+--     'folke/noice.nvim',
+--     event = 'VeryLazy',
+--     dependencies = {
+--       'MunifTanjim/nui.nvim',
+--       'rcarriga/nvim-notify',
+--     },
+--     opts = {
+--       lsp = {
+--         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+--         override = {
+--           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+--           ['vim.lsp.util.stylize_markdown'] = true,
+--           ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+--         },
+--       },
+--       -- you can enable a preset for easier configuration
+--       presets = {
+--         bottom_search = true, -- use a classic bottom cmdline for search
+--         command_palette = true, -- position the cmdline and popupmenu together
+--         long_message_to_split = true, -- long messages will be sent to a split
+--         inc_rename = false, -- enables an input dialog for inc-rename.nvim
+--         lsp_doc_border = false, -- add a border to hover docs and signature help
+--       },
+--     },
+--     -- stylua: ignore
+--     keys = {
+--       { "<S-Enter>",  function() require("noice").redirect(vim.fn.getcmdline()) end,                mode = "c",                 desc = "Redirect Cmdline" },
+--       { "<leader>ul", function() require("noice").cmd("last") end,                                  desc = "[U]I [L]ast Message" },
+--       { "<leader>uh", function() require("noice").cmd("history") end,                               desc = "[U]I [H]istory" },
+--       { "<leader>ua", function() require("noice").cmd("all") end,                                   desc = "[U]I [A]ll" },
+--       -- { "<leader>ud", function() require("noice").cmd("dismiss") end,                                desc = "[U]I [D]ismiss All" },
+--       -- { "<c-f>",       function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,              expr = true,              desc = "Scroll forward",  mode = { "i", "n", "s" } },
+--       -- { "<c-b>",       function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,              expr = true,              desc = "Scroll backward", mode = { "i", "n", "s" } },
+--     },
+--   },
+-- }
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/plugins/notify.lua
@@ -0,0 +1,39 @@
+return {
+  'rcarriga/nvim-notify',
+  lazy = true,
+  keys = {
+    {
+      '<leader>ud',
+      function()
+        require('notify').dismiss { silent = true, pending = true }
+      end,
+      desc = '[U]I [D]ismiss all Notifications',
+    },
+  },
+  config = function()
+    require('notify').setup {
+      stages = 'fade',
+      timeout = 5000,
+      background_colour = '#000000',
+      icons = {
+        ERROR = '',
+        WARN = '',
+        INFO = '',
+        DEBUG = '',
+        TRACE = '',
+      },
+    }
+  end,
+  opts = {
+    timeout = 3000,
+    max_height = function()
+      return math.floor(vim.o.lines * 0.75)
+    end,
+    max_width = function()
+      return math.floor(vim.o.columns * 0.75)
+    end,
+    on_open = function(win)
+      vim.api.nvim_win_set_config(win, { zindex = 100 })
+    end,
+  },
+}
