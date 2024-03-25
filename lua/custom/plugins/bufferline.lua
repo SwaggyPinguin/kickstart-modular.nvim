51a755e64c383717a18e21e79f42ed9ba9cc8370
Noah Dahms <noahdahms@gmail.com>
2024-03-25 14:19:22 +0100

    fixed keymap descriptions

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -28,28 +28,28 @@ return {
     end
 
     -- Tab/Buffers navigation
-    map('n', '<A-,>', ':tabnext<CR>', opts, { desc = { '[T]ab Next' } })
-    map('n', '<A-.>', ':tabprevious<CR>', opts, { desc = { '[T]ab Previous' } })
-    map('n', '<A-c>', ':tabclose<CR>', opts, { desc = { '[T]ab Close' } })
+    map('n', '<A-,>', ':tabnext<CR>', opts, { desc = '[T]ab Next' })
+    map('n', '<A-.>', ':tabprevious<CR>', opts, { desc = '[T]ab Previous' })
+    map('n', '<A-c>', ':tabclose<CR>', opts, { desc = '[T]ab Close' })
 
-    map('n', '<tab>', ':BufferLineCycleNext<CR>', opts, { desc = { '[B]uffer Next' } })
-    map('n', '<S-tab>', ':BufferLineCyclePrev<CR>', opts, { desc = { '[B]uffer Previous' } })
+    map('n', '<tab>', ':BufferLineCycleNext<CR>', opts, { desc = '[B]uffer Next' })
+    map('n', '<S-tab>', ':BufferLineCyclePrev<CR>', opts, { desc = '[B]uffer Previous' })
 
-    map('n', '<A->>', ':BufferLineMoveNext<CR>', opts, { desc = { '[B]uffer Move Next' } })
-    map('n', '<A-<>', ':BufferLineMovePrev<CR>', opts, { desc = { '[B]uffer Move Previous' } })
+    map('n', '<A->>', ':BufferLineMoveNext<CR>', opts, { desc = '[B]uffer Move Next' })
+    map('n', '<A-<>', ':BufferLineMovePrev<CR>', opts, { desc = '[B]uffer Move Previous' })
 
     -- map('n', '<leader>bgt', ':BufferLineGroupToggle<CR>', opts, { desc = { '[B]uffer [G]roup [T]oggle' } })
     -- map('n', '<leader>bgc', ':BufferLineGroupClose<CR>', opts, { desc = { '[B]uffer [G]roup [T]oggle' } })
 
-    map('n', '<leader>bsd', ':BufferLineSortByDirectory<CR>', opts, { desc = { '[B]uffer [S]ort by [D]irectory' } })
-    map('n', '<leader>bse', ':BufferLineSortByExtension<CR>', opts, { desc = { '[B]uffer [S]ort by [E]xtension' } })
-    map('n', '<leader>bst', ':BufferLineSortByTabs<CR>', opts, { desc = { '[B]uffer [S]ort by [T]abs' } })
+    map('n', '<leader>bsd', ':BufferLineSortByDirectory<CR>', opts, { desc = '[B]uffer [S]ort by [D]irectory' })
+    map('n', '<leader>bse', ':BufferLineSortByExtension<CR>', opts, { desc = '[B]uffer [S]ort by [E]xtension' })
+    map('n', '<leader>bst', ':BufferLineSortByTabs<CR>', opts, { desc = '[B]uffer [S]ort by [T]abs' })
 
-    map('n', '<leader>br', ':BufferLineTabRename', {}, { desc = { '[B]uffer [R]ename' } })
-    map('n', '<leader>bd', ':bdelete<CR>', opts, { desc = { '[B]uffer [D]elete' } })
-    map('n', '<leader>bp', ':BufferLineTogglePin<CR>', opts, { desc = { '[B]uffer Toggle [P]in' } })
+    map('n', '<leader>br', ':BufferLineTabRename', {}, { desc = '[B]uffer [R]ename' })
+    map('n', '<leader>bd', ':bdelete<CR>', opts, { desc = '[B]uffer [D]elete' })
+    map('n', '<leader>bp', ':BufferLineTogglePin<CR>', opts, { desc = '[B]uffer Toggle [P]in' })
 
-    map('n', '<C-p>', ':BufferLinePick<CR>', opts, { desc = { '[B]uffer [P]ick' } })
+    map('n', '<C-p>', ':BufferLinePick<CR>', opts, { desc = '[B]uffer [P]ick' })
 
     require 'custom.utils.bufferline.tabconfig'
   end,
