eaceefd7ba1f23a89f593d3ae3d38bdae20d5a41
Noah Dahms <noahdahms@gmail.com>
2024-03-25 14:20:58 +0100

    added keymap group names

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -23,7 +23,9 @@ return {
       -- Document existing key chains
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument | [D]ebug', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>dd'] = { name = '[D]ocument [D]ebug', _ = 'which_key_ignore' },
+        ['<leader>dF'] = { name = '[D]ocument [F]ormat', _ = 'which_key_ignore' },
         ['<leader>ds'] = { name = '[D]ocument [S]ymbols', _ = 'which_key_ignore' },
         ['<leader>dt'] = { name = '[D]ocument [T]wilight', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
@@ -37,8 +39,9 @@ return {
         ['<leader>n'] = { name = '[N]eoTree', _ = 'which_key_ignore' },
         ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
         ['<leader>gh'] = { name = '[G]it [H]unk', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        ['<leader>gt'] = { name = '[G]it [T]oggle', _ = 'which_key_ignore' },
         ['<leader>x'] = { name = 'Trouble', _ = 'which_key_ignore' },
+        ['<leader>u'] = { name = 'Notifications', _ = 'which_key_ignore' },
         ['gp'] = { name = '[G]oto [P]review', _ = 'which_key_ignore' },
       }
     end,
