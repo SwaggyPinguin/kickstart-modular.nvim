ffdcb6df8e315bce6278a2605c0897cdf43a5899
Noah Dahms <noahdahms@gmail.com>
2024-04-03 16:05:10 +0200

    added hlchunk plugin, but disabled

---
+++ lua/custom/plugins/hlchunk.lua
@@ -0,0 +1,21 @@
+return {}
+-- return {
+--   'shellRaining/hlchunk.nvim',
+--   event = { 'UIEnter' },
+--   opts = {
+--     chunk = {
+--       enable = true,
+--     },
+--     indent = {
+--       enable = true,
+--     },
+--     line_num = {
+--       enable = false,
+--       use_treesitter = true,
+--     },
+--     blank = {
+--       enable = false,
+--     },
+--   },
+--   keys = {},
+-- }
