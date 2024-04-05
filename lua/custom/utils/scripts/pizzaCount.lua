86677e840d5914329639436f3e734b9f1e289eb3
Noah Dahms <noahdahms@gmail.com>
2024-04-05 15:33:00 +0200

    strip texts and make lowercase in pizza filter

--- lua/custom/utils/scripts/pizzaCount.lua
+++ lua/custom/utils/scripts/pizzaCount.lua
@@ -29,6 +29,8 @@ vim.api.nvim_create_user_command('FilterPizza', function()
   for _, line in ipairs(lines) do
     local quantity, item = line:match '(%d+)x%s*(.+)'
     if quantity and item then
+      -- strip leading and trailing whitespace and convert item to lowercase
+      item = item:match('^%s*(.-)%s*$'):lower()
       quantity = tonumber(quantity)
       numbers[item] = (numbers[item] or 0) + quantity
       total_sum = total_sum + quantity
@@ -37,6 +39,10 @@ vim.api.nvim_create_user_command('FilterPizza', function()
 
   -- Append the sum of numbers for each item to the end of the file
   for item, sum in pairs(numbers) do
+    -- convert the first letter of each word in item to uppercase
+    item = item:gsub("(%a)([%w_']*)", function(first, rest)
+      return first:upper() .. rest:lower()
+    end)
     vim.api.nvim_buf_set_lines(0, -1, -1, true, { sum .. 'x ' .. item })
   end
 
