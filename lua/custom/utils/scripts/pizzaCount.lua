85b415f64830d8db496df00597f65cc7af071d16
Noah Dahms <noahdahms@gmail.com>
2024-04-12 13:33:15 +0200

    removed unused code

--- lua/custom/utils/scripts/pizzaCount.lua
+++ lua/custom/utils/scripts/pizzaCount.lua
@@ -1,26 +1,4 @@
 vim.api.nvim_create_user_command('FilterPizza', function()
-  -- local numbers = {}
-  -- local total_sum = 0
-  -- local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-  --
-  -- -- Process each line to extract the item and its corresponding quantity
-  -- for _, line in ipairs(lines) do
-  --   local quantity, item = line:match '(%d+)x%s*(.+)'
-  --   if quantity and item then
-  --     quantity = tonumber(quantity)
-  --     numbers[item] = (numbers[item] or 0) + quantity
-  --     total_sum = total_sum + quantity
-  --   end
-  -- end
-  --
-  -- -- Print the sum of numbers for each item
-  -- for item, sum in pairs(numbers) do
-  --   print(sum .. 'x ' .. item)
-  -- end
-  --
-  -- -- Print total sum
-  -- print('\nGesamt: ' .. total_sum)
-
   local numbers = {}
   local total_sum = 0
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
